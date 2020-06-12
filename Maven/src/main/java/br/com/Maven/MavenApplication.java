2fe7515a6fe62df7e933f647e3b1130db5c766e2
rafaelwitter <rafawitt@hotmail.com>
2020-06-12 01:57:03 -0300

    Adicionado dois projetos. Iniciados com Maven APACHE e Gradle

---
+++ Maven/src/main/java/br/com/Maven/MavenApplication.java
@@ -0,0 +1,13 @@
+package br.com.Maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApplication.class, args);
+	}
+
+}
