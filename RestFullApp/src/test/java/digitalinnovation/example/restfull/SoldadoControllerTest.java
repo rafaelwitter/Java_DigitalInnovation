dbdd6670c6a72ef3f6757d455d713e5a4cce1789
rafaelwitter <rafawitt@hotmail.com>
2020-06-24 12:28:09 -0300

    Atualizado o projeto final RestFullApp e criado um projeto novo testesComJava para aprender testes com JAVA.

--- RestFullApp/src/test/java/digitalinnovation/example/restfull/SoldadoControllerTest.java
+++ RestFullApp/src/test/java/digitalinnovation/example/restfull/SoldadoControllerTest.java
@@ -12,22 +12,16 @@ import org.junit.runner.RunWith;
 import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.mockito.Mock;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = SoldadoController.class)
---
+++ testesComJava/README.md
@@ -0,0 +1,1 @@
+### Esse projeto é um fork do repositorio original, criado por Rodrigo Tassini, professor da Digital Innovation One no curso "Aprenda a aplicar testes com Java". Foi usado para o aprendizado de como fazer testes unitários.
